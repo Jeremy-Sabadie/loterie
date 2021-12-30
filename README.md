f4a0782d99b0d989189eb7ee062e480e370b4b2f
John Doe <sabadie.jeremy@gmail.com@gmail.com>
2021-12-30 17:50:23 +0100

    description project in read me

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# loterie
-# loterie
-# loterie
+#principe:
+le principe serai de comparer les numéro des tikets de la personne avec les numéros gagnantsdes précédants tirages.
+# pseudo code loterie:
+deux inputs
+Un pour récupérer tous les numéros gagants.
+Un autre pour récupérer nos numéros.
+Deux boutons.
+Un pour enregistrer le numéro entré soit dans le tableau des numéros gagnants.
+Un autre pour vérifier si un des numéro se trouve dans les deux tableaux en même temps.
+Il y aura donc trois fonction.
+Une pour enregistrer les numéros gagnants.
+Une pour enregistrer nos numéros.
+Une autre pour vérifier s'il y a une concordance et l'afficher dans la page.
--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 
 //ici on récupère les diférents éléments du DOM.
-var WinnerNumbers = document.getElementById('win-numbers');
-var MyNumbers = document.getElementById('my-numbers');
-const VerifyButon = document.getElementById('verify');
-const RegisterButon = document.getElementById('register');
+var WinnerNumbers = document.getElementById('win-numbers');//input pour entrer les numéros gagnants.
+var MyNumbers = document.getElementById('my-numbers');//input pour entrer mes numéros.
+const VerifyButon = document.getElementById('verify');//bouton de vérification.
+const WinRegisterButon = document.getElementById('win-register');//bouton pour enregistrer le numéro gagnant entré.
+const MyRegisterButon = document.getElementById('my-register');//bouton pour enregistrer le notre numéro entré.
+constMessage = document.getElementsByName('h1');
 //la fonction stock récupèrera les valeurs entrées dans les inputs et les tockera dans des tableaux.
-function Stock(){
-    var WinnerArrayNumbers = [];//ce tableau est initialisé vide pour y stocker les numéros rentés.
-    var MyNumbersArray =[];//ce tableau est initialisé vide pour y stocker les numéros rentés.
+function StockWinner(){
+    var WinnerNumbersArray = [];//ce tableau est initialisé vide pour y stocker les numéros gagnantsrentés.
+   
 
 
-    MyNumbersArray.push(MyNumbers.value);//On stocke la valeur de l'input dans le tableau.
-    WinnerArrayNumbers.push(WinnerNumbers.value);//On stocke la valeur de l'input dans le tableau.
-    console.log(WinnerArrayNumbers);
-console.log(MyNumbersArray  );
+    
+    WinnerNumbersArray.push(WinnerNumbers.value);//On stocke la valeur de l'input dans le tableau.
+    WinnerNumbersArray.push(WinnerNumbers.value);//On stocke la nouvelle valeur de l'input dans le tableau.
+    console.log(WinnerNumbersArray);
+
 }
-RegisterButon.onclick=Stock //lors du clic sur le bouton enregistrer on exécute la fonction stock.
+function MyStockNumber(){
+    var MyNumbersArray = [];//Ce tableau vide servira à stocker nos numéros.
+    MyNumbersArray.push(MyNumbers.value); //On stocke la valeur de l'input dans le tableau.
+    MyNumbersArray.push(MyNumbers.value); //On stocke la nouvelle valeur de l'input dans le tableau.
+    console.log(MyNumbersArray);
+}
+function verify(){
+    GlobalTable = MyNumbersArray.concat(WinnerNumbersArray);
+    console.log(GlobalTable);
+    GlobalTable.forEach(element =>console.log(element));
+    //if(ily a concordance){
+        //message.innerHHTML('numéro gagnant')
+    }
+        
+    
+WinRegisterButon.onclick=StockWinner
+MyRegisterButon.onclick=MyStockNumber
+VerifyButon.onclick=verify
+
