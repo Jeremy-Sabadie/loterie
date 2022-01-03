46b43f2d1ae43522dcade71fbb45b0aadad3ee90
John Doe <sabadie.jeremy@gmail.com@gmail.com>
2022-01-03 16:30:49 +0100

    comparison of tables

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ var MyNumbers = document.getElementById('my-numbers');//input pour entrer mes nu
 const VerifyButon = document.getElementById('verify');//bouton de vérification.
 const WinRegisterButon = document.getElementById('win-register');//bouton pour enregistrer le numéro gagnant entré.
 const MyRegisterButon = document.getElementById('my-register');//bouton pour enregistrer le notre numéro entré.
-constMessage = document.getElementsByName('h1');
+const Message = document.querySelectorAll('h1');
 var MyNumbersArray = [];//Ce tableau vide servira à stocker nos numéros.
 var WinnerNumbersArray = [];//ce tableau est initialisé vide pour y stocker les numéros gagnantsrentés.
 //la fonction stock récupèrera les valeurs entrées dans les inputs et les tockera dans des tableaux.
+
 function StockWinner(){
     
    
@@ -17,25 +18,43 @@ function StockWinner(){
     
     WinnerNumbersArray.push(WinnerNumbers.value);//On stocke la valeur de l'input dans le tableau.
     
-    console.log(WinnerNumbersArray);
+    console.log('les numéros gagnants sont : '+WinnerNumbersArray);
 
 }
 function MyStockNumber(){
     
     MyNumbersArray.push(MyNumbers.value); //On stocke la valeur de l'input dans le tableau.
     
-    console.log(MyNumbersArray);
+    console.log('mes numéros  sont :'+MyNumbersArray);
+    
+
 }
-function verify(){
-    GlobalTable = MyNumbersArray.concat(WinnerNumbersArray);
+  var CompareArrays = WinnerNumbersArray===MyNumbersArray;
+    
+    function verify(){
+        if(CompareArrays==true){
+            alert('win');
+            Message.innerHTML='you win';
+        }
+        else{
+            alert('loose');
+            Message.innerHTML='loose';
+        }
+
+
+
     /*console.log(GlobalTable);
     GlobalTable.forEach(element =>console.log(element));
     if(ily a concordance){
         message.innerHHTML('numéro gagnant')*/
-    }
+}
         
-    
+
+   
 WinRegisterButon.onclick=StockWinner
 MyRegisterButon.onclick=MyStockNumber
 VerifyButon.onclick=verify
+
+    
+
 
