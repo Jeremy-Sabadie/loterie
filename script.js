1094084ccb82f0d0c3698b4756bdf80280f72867
John Doe <sabadie.jeremy@gmail.com@gmail.com>
2022-01-13 16:38:55 +0100

    indentation and last rectifications

--- script.js
+++ script.js
@@ -1,105 +1,74 @@
 //début de la récupération des élément de notre côté.
-var MyNumberInput = document.querySelector('#my-numbers');
+var MyNumberInput = document.querySelector("#my-numbers");
 
-const MyRegisterButton =document.querySelector('#my-register');
-var MyListNumber = document.querySelector('#my-list');
+const MyRegisterButton = document.querySelector("#my-register");
+var MyListNumber = document.querySelector("#my-list");
 //début de la récupération des élément du côté des nombres gagnants.
-var WinNumberInput = document.querySelector('#winner-numbers');
+var WinNumberInput = document.querySelector("#winner-numbers");
 //récupération des éléléments de vérifiation
-const WinRegisterButton =document.querySelector('#win-register');
-var WinListNumber = document.querySelector('#win-list');
-const VerifyButton = document.querySelector('#reponse');
-const TextResult = document.querySelector('#result');
-const BtnReset = document.querySelector('#reset');
-TheWinnerNumber=[];
+const WinRegisterButton = document.querySelector("#win-register");
+var WinListNumber = document.querySelector("#win-list");
+const VerifyButton = document.querySelector("#reponse");
+const TextResult = document.querySelector("#result");
+const BtnReset = document.querySelector("#reset");
+TheWinnerNumber = [];
 //fin de la récupération des différents éléments.result-container
 var MyNumbersArray = [];
 var WinNumbersArray = [];
 
+function MyStock() {
+  if (parseInt(MyNumberInput.value)) {
+    MyNumbersArray.join("|");
+    MyNumbersArray.push(parseInt(MyNumberInput.value));
+  }
 
-
-
-
-
-
-function MyStock(){
-    if(parseInt(MyNumberInput.value)){
-      MyNumbersArray.join('|');
-    MyNumbersArray.push((parseInt(MyNumberInput.value)));}
-    
-    MyListNumber.innerHTML= "vos numéros sont : "+MyNumbersArray;
-    MyNumberInput.value="";
-    
+  MyListNumber.innerHTML = "vos numéros sont : " + MyNumbersArray;
+  MyNumberInput.value = "";
 }
-function WinStock(){
-    if(parseInt(WinNumberInput.value)){
-      WinNumbersArray.push((parseInt(WinNumberInput.value)));}
-    
-    WinListNumber.innerHTML="les numéros tirés sont : "+WinNumbersArray;
-    WinNumberInput.value="";
+function WinStock() {
+  if (parseInt(WinNumberInput.value)) {
+    WinNumbersArray.push(parseInt(WinNumberInput.value));
+  }
+
+  WinListNumber.innerHTML = "les numéros tirés sont : " + WinNumbersArray;
+  WinNumberInput.value = "";
 }
 
-    
-            
-        
 function TheReponse() {
-  console.log('TestLog');
-    if (MyNumbersArray.length >= 1 && WinNumbersArray >= 1) {
-       TheWinnerNumber = [];
-      Win = false;
-      MyNumbersArray.forEach((element) => {
-        if (WinNumbersArray.includes(element)) {
-          console.log('yes');
-          console.log(element);
-          TextResult.innerHTML =
-          "vous avez gagné avec le numéro: " + element;
-          TextResult.style.color="green";
-          win = true;
-          TheWinnerNumber.push(element);
-        }
-        else{console.log('loose');
-        TextResult.innerHTML = ":(Vous n'avez pas gagné!";
-        TextResult.style.color="red";}
-      });
-      
-        
-        //TheWinnerNumber.push(element);//problème d'accès à l'élément element.
-      
-        
+  console.log("TestLog");
+  if (MyNumbersArray.length >= 1 && WinNumbersArray.length >= 1) {
+    TheWinnerNumber = [];
+    win = false;
+    MyNumbersArray.forEach((element) => {
+      if (WinNumbersArray.includes(element)) {
+        console.log("yes");
+        console.log(element);
 
-
-
+        win = true;
+        TheWinnerNumber.push(element);
       }
-        
-      
-        
+    });
+    if (win == true) {
+      TextResult.innerHTML =
+        "vous avez gagné avec le(s) numéro: " + TheWinnerNumber.join("et");
+    } else {
+      TextResult.innerHTML = ":(Vous n'avez pas gagné!";
+      TextResult.style.color = "red";
     }
-    
-     
-            
-        
-        
-        
-      
 
-        
-        
-        
-
-
-function Reset(){
-    WinNumbersArray=[];
-    MyNumbersArray= [];
-    TextResult.innerHTML="";
-    WinListNumber.innerHTML="";
-    MyListNumber.innerHTML="";
+    //TheWinnerNumber.push(element);//problème d'accès à l'élément element.
+  }
 }
 
+function Reset() {
+  WinNumbersArray = [];
+  MyNumbersArray = [];
+  TextResult.innerHTML = "";
+  WinListNumber.innerHTML = "";
+  MyListNumber.innerHTML = "";
+}
 
-
-
-
-MyRegisterButton.onclick=MyStock;
-WinRegisterButton.onclick=WinStock;
-VerifyButton.onclick=  TheReponse;
-BtnReset.onclick= Reset;
+MyRegisterButton.onclick = MyStock;
+WinRegisterButton.onclick = WinStock;
+VerifyButton.onclick = TheReponse;
+BtnReset.onclick = Reset;
